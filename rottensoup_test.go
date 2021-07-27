d755a6dc03dee6eee3224f049f94bbbd39af3610
Joerg Walter <jwdev@posteo.de>
2021-07-27 13:02:40 +0200

    -added 2 new tests and html test files.

--- rottensoup_test.go
+++ rottensoup_test.go
@@ -3,9 +3,26 @@
 package rottensoup
 
 import (
+	"fmt"
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"os"
+	"path/filepath"
+	"regexp"
 	"testing"
 )
+
+const htmlDir = "test" //directory where the testfiles are at.
+
+func parseTestFile(name string) (*html.Node, error) {
+	path := filepath.Join(htmlDir, name)
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot open test file: %s", err)
+	}
+	defer f.Close()
+	return html.Parse(f)
+}
 
 func TestMatchAttrs(t *testing.T) {
 
@@ -30,5 +47,51 @@ func TestMatchAttrs(t *testing.T) {
 	empty := make([]html.Attribute, 0)
 	if !MatchAttrs(n, empty...) {
 		t.Error("A match against an empty slice should always return true")
+	}
+}
+
+func TestNextElementSibling(t *testing.T) {
+	const testDoc = "test.html"
+	const testID = "siblings"
+
+	root, err := parseTestFile(testDoc)
+	if err != nil {
+		t.Error(err)
+	}
+	ul := ElementByID(root, testID)
+	if ul == nil {
+		t.Errorf("No Element found with id \"%s\"!", testID)
+	}
+	var li [3]*html.Node
+
+	li[0] = FirstElementByTag(ul, atom.Li)
+	li[1] = NextElementSibling(li[0])
+	li[2] = NextElementSibling(li[1])
+
+	for i := 0; i < 3; i++ {
+		e := li[i]
+		if e.FirstChild.Type != html.TextNode {
+			t.Error("Expected text node")
+		}
+		expect := fmt.Sprintf("Sibling %d", i+1)
+		if e.FirstChild.Data != expect {
+			t.Errorf("Expected \"%s\", got \"%s\"", expect, e.FirstChild.Data)
+		}
+	}
+}
+
+func TestElementsByAttrMatch(t *testing.T) {
+	const testDoc = "attr_match.html"
+
+	search := regexp.MustCompile("caption-[a-z]+")
+
+	root, err := parseTestFile(testDoc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	res := ElementsByAttrMatch(root, "class", search)
+	if len(res) != 3 {
+		t.Errorf("Expected 3 matches, got %d", len(res))
 	}
 }
