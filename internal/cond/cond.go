263a83156ef48e2b2eb624739f2801e01370ea03
Joerg Walter <jwdev@posteo.de>
2021-07-29 14:28:02 +0200

    -removed duplicate code by extending the internal package "cond". -ensured that all Element* functions skip other node types during search.

--- internal/cond/cond.go
+++ internal/cond/cond.go
@@ -21,17 +21,61 @@ func AttrValByRegex(nodes *[]*html.Node, namespace, key string, val *regexp.Rege
 	}
 }
 
-func MatchTag(n **html.Node, tag ...atom.Atom) func(*html.Node) bool {
-	return func(node *html.Node) bool {
-		if node.Type != html.ElementNode {
-			return true
+//Returns a function that adds every given node to nodes if it contains all attributes specified in attr.
+//If first is true, search will stop after first match.
+func MatchAttrs(nodes *[]*html.Node, first bool, attr ...html.Attribute) func(*html.Node) bool {
+	return func(n *html.Node) bool {
+		for _, a := range attr {
+			found := false
+			for _, na := range n.Attr {
+				if a == na {
+					found = true
+					break
+				}
+			}
+			if !found {
+				return true
+			}
 		}
+		*nodes = append(*nodes, n)
+		if first {
+			return false
+		}
+		return true
+	}
+}
+
+func MatchTag(nodes *[]*html.Node, first bool, tag ...atom.Atom) func(*html.Node) bool {
+	return func(node *html.Node) bool {
 		for _, t := range tag {
 			if node.DataAtom == t {
-				*n = node
-				return false
+				*nodes = append(*nodes, node)
+				if first {
+					return false
+				}
+				return true
 			}
 		}
 		return true
+	}
+}
+
+/* --- Filters --- */
+
+func TagFilter(tag atom.Atom, f func(*html.Node) bool) func(*html.Node) bool {
+	return func(n *html.Node) bool {
+		if n.DataAtom != tag {
+			return true
+		}
+		return f(n)
+	}
+}
+
+func TypeFilter(t html.NodeType, f func(*html.Node) bool) func(*html.Node) bool {
+	return func(n *html.Node) bool {
+		if n.Type != t {
+			return true
+		}
+		return f(n)
 	}
 }
--- rottensoup.go
+++ rottensoup.go
@@ -25,31 +25,23 @@ func AttrVal(n *html.Node, namespace, key string) string {
 //Executes depth-first search on all child nodes of n, returns the first element found with the given id.
 //If no suitable element was found, nil will be returned.
 func ElementByID(n *html.Node, id string) *html.Node {
-	var elem *html.Node
-	byID := func(n *html.Node) bool {
-		for _, a := range n.Attr {
-			if a.Key == "id" && a.Val == id {
-				elem = n
-				return false
-			}
-		}
-		return true
+	nodes := make([]*html.Node, 0, 1)
+	attr := html.Attribute{Key: "id", Val: id}
+	nav.DFS(n, cond.TypeFilter(html.ElementNode, cond.MatchAttrs(&nodes, true, attr)), nil)
+	if len(nodes) > 0 {
+		return nodes[0]
 	}
-	nav.DFS(n, byID, nil)
-	return elem
+	return nil
 }
 
 //Executes depth-first search on all child nodes of n, returns the first node that matches NodeType t or nil if no such node was found.
 func FirstNodeByType(n *html.Node, t html.NodeType) *html.Node {
 	var match *html.Node
 	pre := func(n *html.Node) bool {
-		if n.Type == t {
-			match = n
-			return false
-		}
-		return true
+		match = n
+		return false
 	}
-	nav.DFS(n, pre, nil)
+	nav.DFS(n, cond.TypeFilter(t, pre), nil)
 	return match
 }
 
@@ -58,7 +50,7 @@ func FirstNodeByType(n *html.Node, t html.NodeType) *html.Node {
 //If no proper element was found, nil will be returned.
 func ElementsByAttrMatch(n *html.Node, namespace, key string, val *regexp.Regexp) []*html.Node {
 	nodes := make([]*html.Node, 0, 10)
-	nav.DFS(n, cond.AttrValByRegex(&nodes, namespace, key, val), nil)
+	nav.DFS(n, cond.TypeFilter(html.ElementNode, cond.AttrValByRegex(&nodes, namespace, key, val)), nil)
 	if len(nodes) == 0 {
 		return nil
 	}
@@ -68,37 +60,19 @@ func ElementsByAttrMatch(n *html.Node, namespace, key string, val *regexp.Regexp
 //Executes depth-first search on all child nodes of n and returns the first element that matches at least one of the given tags.
 //Returns nil if no such element was found.
 func FirstElementByTag(n *html.Node, tag ...atom.Atom) *html.Node {
-	var node *html.Node
-	pre := func(n *html.Node) bool {
-		if n.Type != html.ElementNode {
-			return true
-		}
-		for _, v := range tag {
-			if n.DataAtom == v {
-				node = n
-				return false
-			}
-		}
-		return true
+	nodes := make([]*html.Node, 0, 1)
+	nav.DFS(n, cond.TypeFilter(html.ElementNode, cond.MatchTag(&nodes, true, tag...)), nil)
+	if len(nodes) > 0 {
+		return nodes[0]
 	}
-	nav.DFS(n, pre, nil)
-	return node
+	return nil
 }
 
 //Executes depth-first search on all child nodes of n and returns all elements that match at least one of the given tags.
 //Returns nil if no such element was found.
 func ElementsByTag(n *html.Node, tag ...atom.Atom) []*html.Node {
 	nodes := make([]*html.Node, 0, 10)
-	pre := func(n *html.Node) bool {
-		for _, t := range tag {
-			if n.DataAtom == t {
-				nodes = append(nodes, n)
-				break
-			}
-		}
-		return true
-	}
-	nav.DFS(n, pre, nil)
+	nav.DFS(n, cond.TypeFilter(html.ElementNode, cond.MatchTag(&nodes, false, tag...)), nil)
 	if len(nodes) == 0 {
 		return nil
 	}
@@ -109,25 +83,10 @@ func ElementsByTag(n *html.Node, tag ...atom.Atom) []*html.Node {
 //the given tag and contain all given attributes. Returns nil if no matches were found.
 func ElementsByTagAndAttr(n *html.Node, tag atom.Atom, attr ...html.Attribute) []*html.Node {
 	nodes := make([]*html.Node, 0, 10)
-	pre := func(n *html.Node) bool {
-		if n.Type == html.ElementNode && n.DataAtom == tag {
-			for _, a := range attr {
-				found := false
-				for _, na := range n.Attr {
-					if a == na {
-						found = true
-						break
-					}
-				}
-				if !found {
-					return true
-				}
-			}
-			nodes = append(nodes, n)
-		}
-		return true
+	nav.DFS(n, cond.TypeFilter(html.ElementNode, cond.TagFilter(tag, cond.MatchAttrs(&nodes, false, attr...))), nil)
+	if len(nodes) == 0 {
+		return nil
 	}
-	nav.DFS(n, pre, nil)
 	return nodes
 }
 
@@ -143,30 +102,22 @@ func HasAttr(n *html.Node, namespace, key string) bool {
 
 //Returns true if node n contains all given attributes, returns false otherwise.
 func MatchAttrs(n *html.Node, attr ...html.Attribute) bool {
-	attrs_to_match := make(map[html.Attribute]bool)
-	for _, a := range attr {
-		attrs_to_match[a] = false
+	nodes := make([]*html.Node, 0, 1)
+	cond.MatchAttrs(&nodes, true, attr...)(n)
+	if len(nodes) > 0 {
+		return true
 	}
-
-	for _, a := range n.Attr {
-		if _, ok := attrs_to_match[a]; ok {
-			attrs_to_match[a] = true
-		}
-	}
-
-	for _, v := range attrs_to_match {
-		if !v {
-			return false
-		}
-	}
-	return true
+	return false
 }
 
 //Returns the node's next sibling where at least one of the given tags match. Returns nil if no such node was found.
 func NextSiblingByTag(n *html.Node, tag ...atom.Atom) *html.Node {
-	var node *html.Node
-	nav.Siblings(n, false, cond.MatchTag(&node, tag...), nil)
-	return node
+	nodes := make([]*html.Node, 0, 1)
+	nav.Siblings(n, false, cond.TypeFilter(html.ElementNode, cond.MatchTag(&nodes, true, tag...)), nil)
+	if len(nodes) > 0 {
+		return nodes[0]
+	}
+	return nil
 }
 
 //Returns the node's next sibling that is an element. Returns nil if no such element was found.
